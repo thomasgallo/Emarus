1831e77f6410a98a749823100e6ecb2c5aad087a
thomasgallo <thomas.gallo@gmail.com>
2020-02-06 00:20:02 +0100

    display a rectangle for the goal detection

--- src/visual_node.py
+++ src/visual_node.py
@@ -46,8 +46,6 @@ class image_feature:
 
         # subscribed Topic to get the camera image
         self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback,  queue_size = 1)
-        if VERBOSE :
-            print "subscribed to /raspicam_node/image/compressed"
 
 
     def callback(self, ros_data):
@@ -55,7 +53,7 @@ class image_feature:
 
         #### direct conversion from ROS to CV2
         np_arr = np.fromstring(ros_data.data, np.uint8)
-        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) #red a image from buffer in memory
 
         # Definiton of red color for the mask
         redLower = (161, 155, 84)
@@ -65,17 +63,17 @@ class image_feature:
         greenLower = (40, 40, 40)
         greenUpper = (70, 255, 255)
 
-        #Recuperation des informations de l'image
+        #take the size of the image
         h, w, d = image_np.shape
 
         #Conversion of the RGB image into HSV
-        blurred = cv2.GaussianBlur(image_np, (11, 11), 0)
+        blurred = cv2.GaussianBlur(image_np, (11, 11), 0) #reduce the noise of the image
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
         #Application of the red mask to the image
         mask = cv2.inRange(hsv, redLower, redUpper)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
+        mask = cv2.erode(mask, None, iterations=2) #erodes away the boundaries of foreground objec
+        mask = cv2.dilate(mask, None, iterations=2) #inverse of erose, to increase the object area after erosion
 
         #Application of the green mask to the image
         mask_goal = cv2.inRange(hsv, greenLower, greenUpper)
@@ -83,7 +81,7 @@ class image_feature:
         mask_goal = cv2.dilate(mask_goal, None, iterations=2)
 
         #Find the ball contour
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #find contour in binary image
         cnts = imutils.grab_contours(cnts)
         center_ball = None
 
@@ -97,8 +95,8 @@ class image_feature:
         if len(cnts) > 0:
 			# find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
 			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
+			((x, y), radius) = cv2.minEnclosingCircle(c) #Finds a circle of the minimum area enclosing a 2D point set.
+			M = cv2.moments(c) #Calculates all of the moments up to the third order of a polygon or rasterized shape.
 			center_ball = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 			# only proceed if the radius meets a minimum size
@@ -124,7 +122,10 @@ class image_feature:
     	if len(cnts_goal) > 0:
     		# find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
     		c = max(cnts_goal, key=cv2.contourArea)
-    		((x, y), radius_goal) = cv2.minEnclosingCircle(c)
+
+            #((x, y), radius_goal) = cv2.minEnclosingCircle(c)
+            x,y,w,h = cv2.boundingRect(c) #get bounding rectangle
+
     		M = cv2.moments(c)
     		center_goal = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
@@ -133,7 +134,8 @@ class image_feature:
     			self.visual_msg.error_goal = center_goal[0] - w/2
     			self.visual_msg.goal_seen = True
     			# draw the circle and centroid on the frame
-    			cv2.circle(image_np, (int(x), int(y)), int(radius_goal),(0, 255, 255), 2)
+                cv2.rectangle(image_np,(x,y),(x+w,y+h),(0,255,0),2) #draw a rectangle around the goal
+    			#cv2.circle(image_np, (int(x), int(y)), int(radius_goal),(0, 255, 255), 2)
     			cv2.circle(image_np, center_goal, 5, (0, 0, 255), -1)
     		else:
                     self.visual_msg.error_goal = 0
@@ -141,15 +143,13 @@ class image_feature:
         else:
             self.visual_msg.goal_seen = 0
             self.visual_msg.error_goal = 0
+
         #publish on the visual recognition topic
         self.recognition_pub.publish(self.visual_msg)
 
         #show the result
     	cv2.imshow('window',image_np)
     	cv2.waitKey(2)
-
-
-
 
 def main(args):
     ic = image_feature()
